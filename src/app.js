c249b1823ae6fc943dd5c92b3755442f3d0441c6
Keven Souza <kevenssouza1@gmail.com>
2023-08-19 10:56:55 -0300

    start in views

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
+require("dotenv").config({});
+const path = require("path");
+const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
-const port = 5000;
 
-app.listen(port, () => {
+app.use((req, res, next) => {
+  if (req.path.endsWith("/") && req.path.length > 1) {
+    const newPath = req.path.slice(0, -1);
+    const query = req.url.slice(req.path.length);
+    res.redirect(301, newPath + query);
+  } else {
+    next();
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
   console.log("SERVER IS ON!");
 });
