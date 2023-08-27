915992c22bbbf3a73d32a01e36c18105425d8bc8
Keven Souza <kevenssouza1@gmail.com>
2023-08-27 19:29:41 -0300

    create user signup function

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,26 +1,39 @@
 import userModel from "../models/user";
 import { Request, Response } from "express";
+import { genSalt, hash } from "bcrypt";
 
 export default class userController {
   static async signup(req: Request, res: Response) {
     try {
-
       const { username, password, confirmPassword } = req.body;
 
       if (!username || !password || !confirmPassword) {
-        return res.status(422).json("there are required fields no filled in!");
+        res.status(422).json("there are required fields no filled in!");
       }
 
       if (password != confirmPassword) {
-        return res.status(422).json("password doesn't match confirmation!");
+        res.status(422).json("password doesn't match confirmation!");
       }
 
-      const users = await userModel.findOne({username: username})
+      const users = await userModel.findOne({ username: username });
 
       if (users) {
-        return res.status(422).json("there is already a has user registred with this username!")
+        res
+          .status(422)
+          .json("there is already a has user registred with this username!");
       }
-      
+
+      const salt = await genSalt(12);
+      const passwordHash = await hash(password, salt);
+
+      const user = new userModel({
+        username: username,
+        password: passwordHash,
+      });
+
+      const response = await userModel.create(user);
+
+      res.status(201).json(response);
     } catch (error) {
       res.status(500).json(error);
     }
