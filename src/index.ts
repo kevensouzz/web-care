6e1e719774766f49eb94596b682ee770a9a2cfdf
kevensouzz <souzadossantoskeven@gmail.com>
2023-09-16 20:59:15 -0300

    update path's redirect

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/router";
 import errorMiddleware from "./middlewares/error";
