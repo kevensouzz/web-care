6bd9991a24e71525f5201e0a89c49f5b187f4bde
kevensouzz <souzadossantoskeven@gmail.com>
2023-09-18 19:57:14 -0300

    create form's fetch function

---
+++ src/public/script/signin.js
@@ -0,0 +1,19 @@
+const form = document.getElementsByTagName("form")[0];
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+
+  fetch("https://reqres.in/api/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+  .then((res) => res.json()
+  .then((data) => console.log(data))
+  );
+});
