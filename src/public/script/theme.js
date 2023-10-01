41cc72b44ec62edf7bdfaf486735a4e2277c392d
kevensouzz <souzadossantoskeven@gmail.com>
2023-10-01 09:08:19 -0300

    add animations on scrolling

--- src/public/script/theme.js
+++ src/public/script/theme.js
@@ -10,47 +10,47 @@ document.getElementById("theme").addEventListener("click", () => {
 
 function setTheme(theme) {
   if (theme == 'Dark') {
-    localStorage.setItem('Tema', theme);
-
+    localStorage.setItem('Theme', theme);
     document.getElementById("favicon").href = "icon/blue-icon.ico"
-
     document.getElementById("logo").src = "icon/blue-icon-32x32.png"
-
     themeIcon.classList.remove("fa-sun");
     themeIcon.classList.add("fa-moon");
-
     document.documentElement.style.setProperty('--color', '#fff');
     document.documentElement.style.setProperty('--background', '#000');
     document.documentElement.style.setProperty('--primary', '#4b4bff');
     document.documentElement.style.setProperty('--secondary', '#ff4b4b');
 
-    document.getElementById("welcome-image").src = "svg/blue-care.svg"
-    document.getElementById("invite-image").src = "svg/blue-signup.svg"
-    document.getElementById("us-image").src = "svg/blue-us.svg"
+    try {
+      document.getElementById("welcome-image").src = "svg/blue-care.svg"
+      document.getElementById("invite-image").src = "svg/blue-signup.svg"
+      document.getElementById("us-image").src = "svg/blue-us.svg"
+    } catch (error) {
+      console.clear();
+    }
   }
   if (theme == 'Light') {
-    localStorage.setItem('Tema', theme);
-
+    localStorage.setItem('Theme', theme);
     document.getElementById("favicon").href = "icon/red-icon.ico"
-
     document.getElementById("logo").src = "icon/red-icon-32x32.png"
-
     themeIcon.classList.add("fa-sun");
     themeIcon.classList.remove("fa-moon");
-
     document.documentElement.style.setProperty('--color', '#000');
     document.documentElement.style.setProperty('--background', '#fff');
     document.documentElement.style.setProperty('--primary', '#ff4b4b');
     document.documentElement.style.setProperty('--secondary', '#4b4bff');
 
-    document.getElementById("welcome-image").src = "svg/red-care.svg"
-    document.getElementById("invite-image").src = "svg/red-signup.svg"
-    document.getElementById("us-image").src = "svg/red-us.svg"
+    try {
+      document.getElementById("welcome-image").src = "svg/red-care.svg"
+      document.getElementById("invite-image").src = "svg/red-signup.svg"
+      document.getElementById("us-image").src = "svg/red-us.svg"
+    } catch (error) {
+      console.clear();
+    }
   }
 }
 
-if (localStorage.getItem('Tema') == undefined) {
+if (localStorage.getItem('Theme') == undefined) {
   setTheme('Dark');
 } else {
-  setTheme(localStorage.getItem('Tema'));
+  setTheme(localStorage.getItem('Theme'));
 }
